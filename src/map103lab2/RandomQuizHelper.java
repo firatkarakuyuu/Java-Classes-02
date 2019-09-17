b619187340057104ab960193ba1c4054d7c9c1eb
19062772 <19062772@B105518.saultcollege.ca>
2019-09-17 10:29:13 -0400

    Initial commit

---
+++ src/map103lab2/RandomQuizHelper.java
@@ -0,0 +1,28 @@
+package map103lab2;
+
+public class RandomQuizHelper {
+private String [] questions = new String [5];
+private String [] answers = new String [5];
+
+RandomQuizHelper () {
+    questions [0] = "What is the biggest country in the world?";
+    questions [1] = "What is the smallest country in the world?";
+    questions [2] = "How much it cost to buy a new Ferrari ?";
+    questions [3] = "What is the friendly country in the world?";
+    questions [4] = "Which one is better to use for home users macOs or Windows ?";
+
+    answers [0] = "Russia";
+    answers [1] = "Vatican City";
+    answers [2] = "$252,800";
+    answers [3] = "Canada";
+    answers [4] = "Windows ?";
+}
+public boolean getResult (int resultIndex ,String answers)
+        
+public String getQuestion(int getIndexResult){
+   
+    return questions (getIndexResult);
+}
+
+private String getResult (int resultIndex){
+}
