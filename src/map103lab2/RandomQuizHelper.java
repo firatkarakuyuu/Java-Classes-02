e167b6a83499d486a2b585a5d9fd3d296453c6ae
19062772 <19062772@B105518.saultcollege.ca>
2019-09-17 10:58:52 -0400

    Initial commit

--- src/map103lab2/RandomQuizHelper.java
+++ src/map103lab2/RandomQuizHelper.java
@@ -1,28 +1,37 @@
 package map103lab2;
 
 public class RandomQuizHelper {
-private String [] questions = new String [5];
-private String [] answers = new String [5];
 
-RandomQuizHelper () {
-    questions [0] = "What is the biggest country in the world?";
-    questions [1] = "What is the smallest country in the world?";
-    questions [2] = "How much it cost to buy a new Ferrari ?";
-    questions [3] = "What is the friendly country in the world?";
-    questions [4] = "Which one is better to use for home users macOs or Windows ?";
+    private String[] questions = new String[5];
+    private String[] answers = new String[5];
 
-    answers [0] = "Russia";
-    answers [1] = "Vatican City";
-    answers [2] = "$252,800";
-    answers [3] = "Canada";
-    answers [4] = "Windows ?";
-}
-public boolean getResult (int resultIndex ,String answers)
+    RandomQuizHelper() {
+        questions[0] = "What is the biggest country in the world?";
+        questions[1] = "What is the smallest country in the world?";
+        questions[2] = "How much it cost to buy a new Ferrari ?";
+        questions[3] = "What is the friendly country in the world?";
+        questions[4] = "Which one is better to use for home users macOs or Windows ?";
+
+        answers[0] = "Russia";
+        answers[1] = "Vatican City";
+        answers[2] = "$252,800";
+        answers[3] = "Canada";
+        answers[4] = "Windows";
+    }
+
+    public boolean getResult(int resultIndex, String answer) {
+
+        if ( answers[resultIndex].equals(answer) ){
+        return true;
+        } else {
+             return false;
+             }
         
-public String getQuestion(int getIndexResult){
-   
-    return questions (getIndexResult);
-}
+    }
 
-private String getResult (int resultIndex){
+    public String getQuestion(int getIndexResult) {
+
+        return questions[getIndexResult];
+    }
+
 }
